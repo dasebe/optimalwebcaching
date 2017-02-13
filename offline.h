fcbb647d9356148dc5316339e27a4f896395955a
Daniel S Berger <berger@cs.uni-kl.de>
2017-02-13 16:09:35 +0100

    added catch test case, and found and fixed a bug

---
+++ offline.h
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <tuple>
+#include <lemon/smart_graph.h>
+
+using namespace lemon;
+
+void parseTraceFile(std::vector<std::tuple<uint64_t,uint64_t,bool> > & trace, std::string & path) {
+    std::ifstream traceFile(path);
+    uint64_t t, id, size, reqc=0, uniqc=0;
+    std::unordered_map<uint64_t, uint64_t> lastSeen;
+
+    while(traceFile >> t >> id >> size) {
+        if(lastSeen.count(id)>0) {
+            std::get<2>(trace[lastSeen[id]]) = true;
+        } else {
+            uniqc++;
+        }
+        trace.emplace_back(id,size,false);
+        lastSeen[id]=reqc++;
+    }
+    std::cout << "scanned trace n=" << reqc << " m= " << uniqc << std::endl;
+}
+                    
+void createMCF(SmartDigraph & g, std::vector<std::tuple<uint64_t,uint64_t,bool> > & trace, uint64_t cacheSize, SmartDigraph::ArcMap<int> & cap, SmartDigraph::ArcMap<double> & cost, SmartDigraph::NodeMap<int> & supplies) {
+
+    // init
+    std::unordered_map<uint64_t, uint64_t> lastSeen;
+    uint64_t reqc=0;
+    SmartDigraph::Arc curArc;
+    SmartDigraph::Node curNode = g.addNode(); // initial node
+    SmartDigraph::Node prevNode;
+    std::unordered_map<uint64_t, uint64_t> lastSize;
+
+    // iterate over trace
+    for(auto it: trace) {
+        // only consider requests that reoccur
+        // should we delete if does not reoccur?
+        if(std::get<2>(it)) {
+            const uint64_t id=std::get<0>(it);
+            const uint64_t size=std::get<1>(it);
+            if(lastSeen.count(id)>0) {
+                if(size==lastSize[id]) {
+                    // create request arc ("outer")
+                    const SmartDigraph::Node lastReq = g.nodeFromId(lastSeen[id]);
+                    curArc = g.addArc(lastReq,curNode);
+                    cap[curArc] = size;
+                    cost[curArc] = 1/static_cast <double>(size);
+                    supplies[lastReq] += size;
+                    supplies[curNode] -= size;
+                } else {
+                    std::cout << "fuckup" << std::endl;
+                    assert(false);
+                }
+            }
+            // create capacity arc ("inner")
+            prevNode = curNode;
+            curNode = g.addNode(); // next node
+            curArc = g.addArc(prevNode,curNode);
+            cap[curArc] = cacheSize; 
+            cost[curArc] = 0;
+            lastSeen[id]=reqc;
+            lastSize[id]=size;
+            reqc++;
+        }
+    }
+    
+    // there will m intervals without arcs
+    // they all intersect and thus point to the same final node
+    for(auto it: lastSeen) {
+        const uint64_t id=it.first;
+        const uint64_t reqc=it.second;
+        const uint64_t size=lastSize[id];
+        const SmartDigraph::Node lastReq = g.nodeFromId(reqc);
+        curArc = g.addArc(lastReq,curNode);
+        cap[curArc] = size;
+        cost[curArc] = 1/static_cast <double>(size);
+        supplies[lastReq] += size;
+        supplies[curNode] -= size;
+    }
+}    
---
+++ tests/test_createMCF.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <list>
+#include <lemon/lgf_writer.h>
+#include "catch.hpp"
+#include "../offline.h"
+
+TEST_CASE( "MCF create function", "[offline]" ) {
+
+    std::vector<std::tuple<uint64_t,uint64_t,bool> > trace;
+    std::string path("test.tr");
+    parseTraceFile(trace, path);
+    REQUIRE(trace.size()==8);
+
+    SmartDigraph g; // mcf graph
+    SmartDigraph::ArcMap<int> cap(g); // mcf capacities
+    SmartDigraph::ArcMap<double> cost(g); // mcf costs
+    SmartDigraph::NodeMap<int> supplies(g); // mcf demands/supplies
+    uint64_t cacheSize = 10;
+    createMCF(g, trace, cacheSize, cap, cost, supplies);
+
+    digraphWriter(g, std::cout).
+        nodeMap("supplies", supplies).
+        arcMap("capacity", cap).       // write cap into 'capacity'
+        arcMap("cost", cost).          // write 'cost' for for arcs
+        run();
+
+    int nodes=0, vertices=0, supplysum=0;
+    for (SmartDigraph::NodeIt n(g); n!=INVALID; ++n) {
+        ++nodes;
+        supplysum+=supplies[n];
+    }
+    REQUIRE(nodes==6);
+    REQUIRE(supplysum==0);
+    
+    for (SmartDigraph::ArcIt a(g); a != INVALID; ++a) {
+        ++vertices;
+    }
+    REQUIRE(vertices==10);
+
+    // check details
+    // Checking supply/demand values
+    REQUIRE(supplies[g.nodeFromId(0)]==2);
+    REQUIRE(supplies[g.nodeFromId(1)]==3);
+    REQUIRE(supplies[g.nodeFromId(2)]==0);
+    REQUIRE(supplies[g.nodeFromId(3)]==4);
+    REQUIRE(supplies[g.nodeFromId(4)]==0);
+    REQUIRE(supplies[g.nodeFromId(5)]==-1*(2+3+4));
+
+    // Checking costs and capacities
+    std::list<uint64_t> cl {0, 1, 3, 4, 6};
+    for(auto it: cl) {
+        REQUIRE(cap[g.arcFromId(it)]==10);
+        REQUIRE(cost[g.arcFromId(it)]==0);
+    }
+    std::list<uint64_t> id1 {2, 5, 9};
+    for(auto it: id1) {
+        REQUIRE(cap[g.arcFromId(it)]==2);
+        REQUIRE(cost[g.arcFromId(it)]==1/2.0);
+    }
+    REQUIRE(cap[g.arcFromId(7)]==4);
+    REQUIRE(cost[g.arcFromId(7)]==1/4.0);
+    REQUIRE(cap[g.arcFromId(8)]==3);
+    REQUIRE(cost[g.arcFromId(8)]==1/3.0);
+}
