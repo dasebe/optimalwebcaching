169a11c5a7acf7680da7ca0b6102dddb052622ba
Daniel S Berger <berger@cs.uni-kl.de>
2017-03-03 14:50:52 +0100

    complete rewrite of localratio using more straightforward n^3 algorithm

--- localratio.cpp
+++ localratio.cpp
@@ -26,8 +26,24 @@ inline void log_message(std::string m, double x, double y, double z, double a, s
     std::cerr << m << "," << x << "," << y  << "," << z << "," << a << e;
 }
 
+struct trEntry {
+    const uint64_t id;
+    const uint64_t size;
+    double pval;
+    size_t nextSeen;
+    bool inSchedule;
+    bool hasNext;
 
-typedef std::tuple<uint64_t,uint64_t,bool,double,size_t> traceEntry; //id, size, hasNext, pvalue, nextSeen (index of next request)
+    trEntry(uint64_t nid, uint64_t nsize)
+        : id(nid),
+          size(nsize)
+    {
+        pval = 1;
+        nextSeen = 0;
+        inSchedule = false;
+        hasNext = false;
+    };
+};
 
 int main(int argc, char* argv[]) {
 
@@ -37,17 +53,15 @@ int main(int argc, char* argv[]) {
 
     // each interval is represented by the index of the trace, where it started
     // store trace here
-    std::vector<traceEntry> trace;
+    std::vector<trEntry> trace;
     // store lastSeen entries here
     std::map<std::pair<uint64_t, uint64_t>, size_t> lastSeen;
-    // intervals that intersect at a time, point to request index
-    std::vector<std::map< std::pair<uint64_t, uint64_t>, size_t > > sameTime;
     // store which intervals are in the schedule
-    std::unordered_set< size_t > inSchedule;
+    std::unordered_set< size_t > scheduleSet;
 
     // open trace file
     std::ifstream traceFile(path);
-    uint64_t time, id, size, reqc=0, uniqc=0;
+    uint64_t time, id, size, reqc=0;
     // scan trace and initialize data structures
     while(traceFile >> time >> id >> size) {
         // only consider objects that are requested at least once (ignore last request, because doesn't make sense to cache that one)
@@ -56,62 +70,35 @@ int main(int argc, char* argv[]) {
             // find trace index where this interval started
             const size_t indexLastSeen = lastSeen[std::make_pair(id,size)];
             // add to schedule the start index of this interval
-            inSchedule.insert(indexLastSeen);
+            // TODO MIGHT NOT NEED THIS
+            scheduleSet.insert(indexLastSeen);
+            trEntry & lastSeenEntry = trace[indexLastSeen];
             // update trace so that we know this interval has finite length
-            std::get<2>(trace[indexLastSeen]) = true;
+            lastSeenEntry.hasNext = true;
             // update trace so that we know the end time of this interval
-            std::get<4>(trace[indexLastSeen]) = reqc;
-            // intersection set of this interval:
-            // store indexLastSeen for every discrete time step this request has spanned
-            for(uint64_t i=indexLastSeen; i<reqc; i++) {
-                (sameTime[i])[std::make_pair(id,size)] = indexLastSeen;
-            }
-        } else {
-            // see for first time, so count unique objects
-            uniqc++;
+            lastSeenEntry.nextSeen = reqc;
+            // update trace: this interval is in schedule
+            lastSeenEntry.inSchedule = true;
         }
         // store: id, size, bool seen before, pvalue, start of this interval (if started)
-        trace.emplace_back(id,size,false,1,0);
-        sameTime.push_back( std::map< std::pair<uint64_t, uint64_t>, size_t >() );
+        trace.emplace_back(id,size);
         lastSeen[std::make_pair(id,size)]=reqc++;
     }
 
+    // free memory
+    lastSeen.clear();
     std::cerr << "parsed trace\n";
     
     // DEBUG print trace
+#ifdef CDEBUG
     reqc = 0;
     LOGnl("TRACE",0,0,0,0);
-    for(traceEntry & thisTrEntry: trace) {
-        const uint64_t id = std::get<0>(thisTrEntry);
-        const uint64_t size = std::get<1>(thisTrEntry);
-        double & pval = std::get<3>(thisTrEntry);
-        const size_t firstSeen = reqc++;
-        const size_t nextSeen = std::get<4>(thisTrEntry);
-        LOGnl(" ",id,size,firstSeen,nextSeen);
+    for(auto & curEntry : trace) {
+        LOGnl(" ",curEntry.id,curEntry.size,0,curEntry.nextSeen);
     }
+#endif
     // END DEBUG
     
-    // initialize time width (total sum of object sizes that span a discrete time step)
-    std::multimap<int64_t, uint64_t > widthToTime;
-    std::unordered_map<uint64_t, std::multimap<int64_t, uint64_t >::iterator > timeToWidthIterator; // reverse mapping
-    uint64_t currentWidth;
-    reqc = 0;
-    for(auto & it: sameTime) {
-        currentWidth = 0;
-        for(auto & vit: it) {
-            const traceEntry & thisTrEntry = trace[vit.second];
-            const uint64_t id = std::get<0>(thisTrEntry);
-            const uint64_t size = std::get<1>(thisTrEntry);
-            LOG("|",id,size,0,0);
-            currentWidth+=size;
-        }
-        const int64_t currentDelta = currentWidth-cacheSize;
-        auto tWIt = widthToTime.emplace(currentDelta, reqc);
-        timeToWidthIterator[reqc] = tWIt;
-        LOGnl("|  TW ",currentDelta,reqc,0,0);
-        reqc++;
-    }
-
 
     // create feasible schedule by excluding worst feasibility violations 
     // i.e., select largest delta in every iteration
@@ -119,161 +106,160 @@ int main(int argc, char* argv[]) {
     // store excluded intervals in a stack for later
     std::stack<size_t> excluded;
 
-    // find largest delta
-    auto widthIt = --widthToTime.end();
-    int64_t deltaStar = widthIt->first;
+    // delta data structures
+    int64_t curDelta;
+    int64_t deltaStar;
+    size_t timeStar;
+    // map: intersecting interval set
+    std::map<std::pair<uint64_t, uint64_t>, size_t> curI; //intersecting intervals at current time
+    std::map<std::pair<uint64_t, uint64_t>, size_t> maxI; // intersectin intervals at timeStar
 
-    // iterate
-    while (deltaStar > 0) {
+    // iterate (every iteration removes one interval, so max iteration count = trace size)
+    for(size_t i=0; i<trace.size(); i++) {
 
-        const uint64_t timeStar = widthIt->second;
-        LOGnl("\nd*,t* ",deltaStar,timeStar,0,0);
+        // find highest delta
+        reqc = 0;
+        // iterate over all time instances (we can skip the last one)
+
+        maxI.clear();
+        curDelta = -cacheSize;
+        deltaStar = -cacheSize;
+        timeStar = 0;
+        for(size_t j=0; j<trace.size(); j++) {
+            trEntry & curEntry = trace[j];
+
+            // if no next request and in intersecting intervals -> remove
+            if(!curEntry.hasNext & curI.count(std::make_pair(curEntry.id,curEntry.size))>0) {
+                curI.erase(std::make_pair(curEntry.id,curEntry.size));
+                curDelta -= curEntry.size;
+                assert(!curEntry.inSchedule);
+            }
+
+            // if in schedule -> update curI
+            if(curEntry.inSchedule) {
+
+                // if not already in current intersecting set
+                if(curI.count(std::make_pair(curEntry.id,curEntry.size))<=0 ) {
+                    curDelta += curEntry.size;
+                } // else: don't need update the size/width
+
+                // add to current intersecting set
+                curI[std::make_pair(curEntry.id,curEntry.size)] = j;
+
+                // check if we need to update deltaStar
+                if(curDelta > deltaStar) {
+                    deltaStar = curDelta;
+                    timeStar = j;
+                    // copy intersecting set
+                    maxI = curI;
+                }
+            }
+
+#ifdef CDEBUG
+            for(auto it: curI) {
+                trEntry & curEntry = trace[it.second];
+                LOG("|",curEntry.id,curEntry.size,0,0);
+            }
+            LOGnl("|  TW ",curDelta,j,deltaStar,timeStar);
+#endif                
+        }
+
+        // check if we found a feasible solution
+        if(deltaStar <= 0)
+            break;
+
+        curI.clear();
+        assert(maxI.size()>0);
+        assert(deltaStar > 0);
+        assert(timeStar > 0); // first time interval can't be the unstable one
+        
+        LOGnl("\nd*,t*",deltaStar,timeStar,0,0);
+        std::cout << "d*" << deltaStar << " t* " << timeStar;
         
         // find smallest rho so that p2 reaches zero
         double rho = 1;
-        for(auto & it : sameTime[timeStar]) {
-            const traceEntry & thisTrEntry = trace[it.second];
-            const uint64_t id = std::get<0>(thisTrEntry);
-            const double size = std::get<1>(thisTrEntry);
-            const double pval = std::get<3>(thisTrEntry);
-            const double thisRho = pval/size;
+        for(auto & vit : maxI) {
+            trEntry & curEntry = trace[vit.second];
+            const double thisRho = curEntry.pval/curEntry.size;
             rho = thisRho <= rho ? thisRho : rho;
         }
         assert(rho < 1);
         LOGnl("min rho ",rho,0,0,0);
-
-        // update p2, exclude p2=0 intervals from schedule
-        for(auto it=sameTime[timeStar].begin(); it!=sameTime[timeStar].end(); ) {
-
-            // define constants with this object's properties
-            traceEntry & thisTrEntry = trace[it->second];
-            const uint64_t id = std::get<0>(thisTrEntry);
-            const uint64_t size = std::get<1>(thisTrEntry);
-            double & pval = std::get<3>(thisTrEntry);
-            const size_t firstSeen = it->second;
-            const size_t nextSeen = std::get<4>(thisTrEntry);
+        std::cout << " mrho " << rho << "\n";
+        
+        // update p2, exclude intervals with p2=0 from schedule
+        for(auto & vit : maxI) {
+            trEntry & curEntry = trace[vit.second];
 
             // p2==0
-            if(pval <= rho * size ) {
-                LOGnl("exclude (t,id,size,p2)",it->second,id,size,pval - rho*size);
-
-                // exclude from schedule and update width  of all times between firstSeen and nextSeen
-                for(size_t i=firstSeen; i<nextSeen; i++) {
-                    // find iterator to widthToTime map and assert it exists
-                    auto wTITold = timeToWidthIterator.find(i);
-                    assert(wTITold != timeToWidthIterator.end());
-                    auto tWItold = timeToWidthIterator[i]; // iterator to this widthToTime field
-                    assert(i==tWItold->second);
-                    // calculate new delta
-                    const int64_t oldDelta = tWItold->first;
-                    const int64_t newDelta = oldDelta-size;
-                    // delete from widthToTime map
-                    widthToTime.erase(tWItold);
-                    // create new entry in widthToTime with new delta value
-                    auto tWItnew = widthToTime.emplace(newDelta, i);
-                    timeToWidthIterator[i] = tWItnew;
-                    // skip current it iterator
-                    if(i!=timeStar) {
-                        (sameTime[i]).erase(std::make_pair(id,size));
-                    }
-                    // added to queue of excluded intervals
-                }
-                // set p2 to 0 by definition
-                pval = 0;
+            if(curEntry.pval <= rho * curEntry.size ) {
+                LOGnl("exclude (t,id,size,p2)",vit.second,id,curEntry.size,curEntry.pval - rho*curEntry.size);
+                curEntry.pval = 0;
+                curEntry.inSchedule = false;
                 // add current interval to excluded ones
-                excluded.push(it->second);
+                excluded.push(vit.second);
                 // delete from current schedule
-                inSchedule.erase(it->second);
-                // delete current entry from current sameTime map
-                it = sameTime[timeStar].erase(it);
+                scheduleSet.erase(vit.second);
             } else {
                 // p2 >= 0
-                pval -= size <= deltaStar ? rho * size : rho * deltaStar; // pval = pval - p1
-                it++; // continue iterator
+                curEntry.pval -= curEntry.size <= deltaStar ? rho * curEntry.size : rho * deltaStar; // pval = pval - p1
             }
         }
-        // find time step with max delta
-        assert(widthToTime.size()>0);
-
-        //debug
-        reqc = 0;
-        for(auto & it: sameTime) {
-            for(auto & vit: it) {
-                const traceEntry & thisTrEntry = trace[vit.second];
-                const uint64_t id = std::get<0>(thisTrEntry);
-                const uint64_t size = std::get<1>(thisTrEntry);
-                LOG("|",id,size,0,0);
-            }
-            LOGnl("|  TW ",timeToWidthIterator[reqc]->first,reqc,0,0);
-            reqc++;
-        }
-        // end of debug
-        
-        widthIt = --widthToTime.end();
-        deltaStar = widthIt->first;
     }
-
+    maxI.clear();
     std::cerr << "found feasible schedule\n";
+    std::cout << "hitc " << scheduleSet.size() << " reqc " << trace.size() << " OHR " << static_cast<double>(scheduleSet.size())/trace.size() << "\n";
 
     // we now have a feasible schedule, which might not be maximal
 
     while (!excluded.empty())
     {
-        traceEntry & thisTrEntry = trace[excluded.top()];
-        const uint64_t id = std::get<0>(thisTrEntry);
-        const uint64_t size = std::get<1>(thisTrEntry);
         const size_t firstSeen = excluded.top();
-        const size_t nextSeen = std::get<4>(thisTrEntry);
-        bool feasibleToAdd = true;
+        trEntry & newEntry = trace[excluded.top()];
+        curI.clear();
+        curDelta = -cacheSize;
+        deltaStar = -cacheSize;
 
-        // check consistency (shouldn't be necessary)
-        assert(inSchedule.find(firstSeen)==inSchedule.end());
+        for(size_t j=0; j<trace.size(); j++) {
+            trEntry & curEntry = trace[j];
 
-        // check feasibility for all time steps
-        for(size_t i=firstSeen; i<nextSeen; i++) {
-            // find iterator to widthToTime map and assert it exists
-            auto wTITold = timeToWidthIterator.find(i);
-            assert(wTITold != timeToWidthIterator.end());
-            auto tWItold = timeToWidthIterator[i]; // iterator to this widthToTime field
-            assert(i==tWItold->second);
-            // calculate new delta
-            const int64_t oldDelta = tWItold->first;
-            const int64_t newDelta = oldDelta+size;
-            if(newDelta > 0) {
-                feasibleToAdd = false;
-                break;
+            // if no next request and in intersecting intervals -> remove
+            if(!curEntry.hasNext & curI.count(std::make_pair(curEntry.id,curEntry.size))>0) {
+                curI.erase(std::make_pair(curEntry.id,curEntry.size));
+                curDelta -= curEntry.size;
+                assert(!curEntry.inSchedule);
+            }
+
+            // if in schedule -> update curI
+            if(j==excluded.top() || curEntry.inSchedule) {
+
+                // if not already in current intersecting set
+                if(curI.count(std::make_pair(curEntry.id,curEntry.size))<=0 ) {
+                    curDelta += curEntry.size;
+                } // else: don't need update the size/width
+
+                // add to current intersecting set
+                curI[std::make_pair(curEntry.id,curEntry.size)] = j;
+
+                // check if we need to update deltaStar
+                if(curDelta > deltaStar) {
+                    deltaStar = curDelta;
+                }
             }
         }
-
-        // actually add this one
-        if(feasibleToAdd) {
-            LOGnl("adding to schedule",firstSeen,0,0,0);
-            // add to feasible schedule
-            inSchedule.insert(firstSeen);
-            // update intersecting time steps
-            for(size_t i=firstSeen; i<nextSeen; i++) {
-                // find iterator to widthToTime map and assert it exists
-                auto wTITold = timeToWidthIterator.find(i);
-                assert(wTITold != timeToWidthIterator.end());
-                auto tWItold = timeToWidthIterator[i]; // iterator to this widthToTime field
-                assert(i==tWItold->second);
-                // calculate new delta
-                const int64_t oldDelta = tWItold->first;
-                const int64_t newDelta = oldDelta+size;
-                // delete from widthToTime map
-                widthToTime.erase(tWItold);
-                // create new entry in widthToTime with new delta value
-                auto tWItnew = widthToTime.emplace(newDelta, i);
-                timeToWidthIterator[i] = tWItnew;
-            }
+        // if still feasible add excluded.top() to schedule
+        if(deltaStar <=0) {
+            newEntry.inSchedule = true;
+            scheduleSet.insert(excluded.top());
         }
-        
+
+        // pop stack
         excluded.pop();
     }
 
     std::cerr << "found maximal schedule\n";
 
-    std::cout << "hitc " << inSchedule.size() << " reqc " << trace.size() << " OHR " << static_cast<double>(inSchedule.size())/trace.size() << "\n";
+    std::cout << "hitc " << scheduleSet.size() << " reqc " << trace.size() << " OHR " << static_cast<double>(scheduleSet.size())/trace.size() << "\n";
     
     return 0;
 }
