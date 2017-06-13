5af7e0120ac3aa8a5940a0de28890759bde2c8de
Daniel S. Berger <berger@cs.uni-kl.de>
2017-06-13 19:40:42 +0200

    frequency based eviction code stump

--- freq_base.cpp
+++ freq_base.cpp
@@ -6,13 +6,13 @@
 #include <queue>
 #include <list>
 #include <tuple>
-#include <assert.h>
+#include <cassert>
 #include <random>
 
 
 using namespace std;
 
-typedef tuple<long, long> object_t; // objectid, size
+typedef tuple<uint64_t, uint64_t> object_t; // objectid, size
 typedef list<object_t>::iterator list_iterator_t;
 typedef multimap<long double, object_t>::iterator map_iterator_t;
 
@@ -28,7 +28,7 @@ typedef multimap<long double, object_t>::iterator map_iterator_t;
 #endif
 
 void log_message(string m, double x, double y, double z) {
-  cerr << m << "," << x << "," << y  << "," << z << "\n";
+    cerr << m << "," << x << "," << y  << "," << z << "\n";
 }
 
 /*
@@ -37,62 +37,62 @@ void log_message(string m, double x, double y, double z) {
 
 class Cache {
 protected:
-  // caching definitions
-  const long long cache_size;
-  long long current_size; // size of the cache
-  // hit statistics
-  long hits;
-  long long bytehits;
+    // caching definitions
+    const uint64_t cache_size;
+    uint64_t current_size; // size of the cache
+    // hit statistics
+    uint64_t hits;
+    uint64_t bytehits;
 
-  bool logStatistics;
-  virtual void hit(long size) {
-    if(logStatistics) {
-      hits++;
-      bytehits+=size;
+    bool logStatistics;
+    virtual void hit(uint64_t size) {
+        if(logStatistics) {
+            hits++;
+            bytehits+=size;
+        }
     }
-  }
 
 public:
-  Cache(long long cs) : cache_size(cs), current_size(0), hits(0), bytehits(0), logStatistics(false) {
-  }
+    Cache(uint64_t cs) : cache_size(cs), current_size(0), hits(0), bytehits(0), logStatistics(false) {
+    }
 
-  virtual ~Cache(){};
+    virtual ~Cache(){};
 
-  virtual bool request (const long cur_req, const long size) {return(false);}
+    virtual bool request (const uint64_t cur_req, const uint64_t size) {return(false);}
 
-  // statistics
-  virtual void startStatistics() {
-    logStatistics = true;
-  }
+    // statistics
+    virtual void startStatistics() {
+        logStatistics = true;
+    }
 
-  virtual void stopStatistics() {
-    logStatistics = false;
-  }
+    virtual void stopStatistics() {
+        logStatistics = false;
+    }
 
-  virtual void resetStatistics() {
-    hits = 0;
-    bytehits = 0;
-  }
+    virtual void resetStatistics() {
+        hits = 0;
+        bytehits = 0;
+    }
   
-  virtual long getHits() const {
-    return(hits);
-  }
+    virtual uint64_t getHits() const {
+        return(hits);
+    }
 
-  virtual bool getLogStatistics() const {
-    return(logStatistics);
-  }
+    virtual bool getLogStatistics() const {
+        return(logStatistics);
+    }
 
-  virtual long long getBytehits() const {
-    return(bytehits);
-  }
+    virtual uint64_t getBytehits() const {
+        return(bytehits);
+    }
 
-  virtual long long getCurrentSize() const {
-    return(current_size);
-  }
+    virtual uint64_t getCurrentSize() const {
+        return(current_size);
+    }
 
-  virtual long long getCacheSize() const {
-    return(cache_size);
-  }
+    virtual uint64_t getCacheSize() const {
+        return(cache_size);
+    }
 };
 
  
@@ -102,54 +102,61 @@ public:
 
 class StaticCache: public Cache {
 protected:
-  unordered_set<long> cache_set;
-  unordered_map<long, object_t> history_map;
+    unordered_set<uint64_t> cache_set;
+    unordered_map<uint64_t, object_t> history_map;
 
 public:
-  StaticCache(long long cs): Cache(cs) {}
+    StaticCache(uint64_t cs): Cache(cs) {}
 
-  ~StaticCache(){}
+    ~StaticCache(){}
 
-  bool request (const long cur_req, const long size) {
-    // gather statistics
-    if (history_map.count(cur_req) == 0)
-      history_map[cur_req] = object_t(1,size);
-    else
-      (get<0>(history_map[cur_req]))++;
-    // check whether in cache
-    if(cache_set.count(cur_req)>0)
-      {
-	Cache::hit(size);
-	return(true);
-      }
-    return(false);
-  }
-
-  void activateHistory () {
-    //    cout << "history activated start\n";
-    map<double, long> history_order;
-    // sort request statistic by weight
-    for (auto ait: history_map)
-      history_order.insert(pair<double,long>(
-					     double(get<0>(ait.second))/get<1>(ait.second), // weight: number of requests/ size
-					     ait.first)); // id
-    // clear old cache
-    cache_set.clear();
-    current_size = 0;
-    cerr << "history size: " << history_order.size() << "\n";
-    while (history_order.size()>0 && current_size <= cache_size) {
-      map<double, long>::iterator highest_weight = (--history_order.end());
-      const long highest_id = highest_weight->second;
-      const long highest_size = get<1>(history_map[highest_id]);
-      if(current_size + highest_size <= cache_size) {
-	current_size += highest_size;
-	cache_set.insert(highest_id);
-      }
-      history_order.erase(highest_weight);
+    bool request (const uint64_t cur_req, const uint64_t size) {
+        // gather statistics
+        if (history_map.count(cur_req) == 0)
+            history_map[cur_req] = object_t(1,size);
+        else
+            (get<0>(history_map[cur_req]))++;
+        // check whether in cache
+        if(cache_set.count(cur_req)>0)
+        {
+            Cache::hit(size);
+            return(true);
+        }
+        return(false);
     }
-    history_map.clear();
-    cerr << "history activated - cache size: " << double(current_size)/cache_size << "\n";
-  }
+
+    bool antiRequest (const uint64_t cur_req, const uint64_t size) {
+        //        cerr << "anti " << cur_req << " " << size << "\n";
+        //        assert(history_map.count(cur_req) > 0);
+        assert(get<0>(history_map[cur_req]) > 0);
+        (get<0>(history_map[cur_req]))--;
+    }
+
+    void activateHistory () {
+        //    cout << "history activated start\n";
+        map<double, uint64_t> history_order;
+        // sort request statistic by weight
+        for (auto ait: history_map)
+            history_order.insert(pair<double,uint64_t>(
+                                                   double(get<0>(ait.second))/get<1>(ait.second), // weight: number of requests/ size
+                                                   ait.first)); // id
+        // clear old cache
+        cache_set.clear();
+        current_size = 0;
+        cerr << "history size: " << history_order.size() << "\n";
+        while (history_order.size()>0 && current_size <= cache_size) {
+            map<double, uint64_t>::iterator highest_weight = (--history_order.end());
+            const uint64_t highest_id = highest_weight->second;
+            const uint64_t highest_size = get<1>(history_map[highest_id]);
+            if(current_size + highest_size <= cache_size) {
+                current_size += highest_size;
+                cache_set.insert(highest_id);
+            }
+            history_order.erase(highest_weight);
+        }
+        //        history_map.clear();
+        cerr << "history activated - cache size: " << double(current_size)/cache_size << "\n";
+    }
 };
 
 
