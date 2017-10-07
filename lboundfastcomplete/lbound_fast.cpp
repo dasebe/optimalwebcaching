bdd14d739ad9c8126126498991460a2bc40c0aa5
Daniel S Berger <dsberger@cmu.edu>
2017-10-07 18:50:18 -0400

    first lboundfastcomplete

---
+++ lboundfastcomplete/lbound_fast.cpp
@@ -0,0 +1,111 @@
+#include <lemon/lgf_writer.h>
+#include <cassert>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+#include "lib/parse_trace.h"
+#include <limits>
+
+using namespace lemon;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cerr << argv[0] << " traceFile" << std::endl;
+        return 1;
+    }
+
+    std::cerr << "starting..\n";
+
+    std::string path(argv[1]);
+
+    // parse trace file
+    std::vector<trEntry> trace;
+    uint64_t uniqCount;
+    uint64_t totalReqc = parseTraceFile(trace, path, uniqCount);
+    std::cerr << "scanned trace n=" << totalReqc << std::endl;
+
+    // ordered list of utilities
+    std::vector<utilEntry> utils;
+    std::cerr << "reserving " <<  trace.size() - uniqCount << " " << trace.size() << " " << uniqCount << "\n";
+    utils.reserve(trace.size() - uniqCount);
+    for(size_t i=0; i<trace.size(); i++) {
+        const trEntry & cur = trace[i];
+        if(cur.nextSeen > 0) {
+            // calculate utility
+            const double intervalLength = cur.nextSeen - i;
+            const double utilityDenominator = cur.size*intervalLength;
+            if(utilityDenominator<=0) {
+                std::cerr << "fail " << cur.size << " ns " << cur.nextSeen << " " << i << " " << utilityDenominator << " " << intervalLength << "\n";
+            }
+            assert(utilityDenominator>0);
+            utils.emplace_back(1.0L/utilityDenominator, i);
+        }
+    }
+    std::sort(utils.begin(), utils.end());
+    std::cerr << "ordered " << utils.size() << " utilities\n";
+
+    // create used-up capacity mask
+    int64_t * usedcap = new int64_t[trace.size()];
+#pragma omp simd
+    for(size_t i=0; i<trace.size(); i++) {
+        usedcap[i] = 0;
+    }
+
+    std::unordered_set<size_t> cached;
+    uint64_t counter = 0;
+
+    for(int64_t cs=1; cached.size() != trace.size(); cs = cs * 2) {
+
+        // safe guard
+        if(cs > std::numeric_limits<int64_t>::max()/2) {
+            break;
+        }
+
+        std::cerr << cs;
+        for (auto it = utils.rbegin(); it != utils.rend(); it++) {
+
+            if(cached.count(it->index)>0) {
+                continue; // already cached
+            }
+            
+            const trEntry & cur = trace[it->index];
+            const auto s = cur.size;
+            if(s > cs) {
+                continue; // skip if too large to store at the moment
+            }
+
+            bool checkedAnything = false;
+            for(size_t i = it->index; i<cur.nextSeen; i++) {
+                checkedAnything = true;
+                if(usedcap[i] + s > cs) {
+                    continue; // skip
+                }
+            }
+
+            if(!checkedAnything) {
+                std::cerr << "\n nothing checked " << it->index << " " << cur.nextSeen << std::endl;
+            }
+
+            // reaching here means we space to store the object
+#pragma omp simd
+            for(size_t i = it->index; i<cur.nextSeen; i++) {
+                usedcap[i] += s;
+            }
+            cached.insert(it->index);
+
+            if(counter++ > 10000) {
+                std::cerr << ".";
+                counter = 0;
+            }
+
+        }
+        std::cerr << std::endl;
+        counter = 0;
+        std::cout << "lboundfastc " + std::to_string(cs) + " " + std::to_string(cached.size()) + " " + std::to_string(totalReqc) +  " " + std::to_string((double)(cached.size())/totalReqc) + "\n";
+    }
+
+    return 0;
+}
---
+++ lboundfastcomplete/lib/parse_trace.cpp
@@ -0,0 +1,60 @@
+//#include <iostream>
+#include <fstream>
+#include <map>
+#include <cassert>
+#include <tuple>
+#include <cmath>
+#include <unordered_set>
+#include "parse_trace.h"
+
+//using namespace lemon;
+
+uint64_t parseTraceFile(std::vector<trEntry> & trace, std::string & path, uint64_t & uniqCount) {
+    std::ifstream traceFile(path);
+    uint64_t time, id, reqc=0, totalreqc=0;
+    int64_t size;
+    std::unordered_map<std::pair<uint64_t, uint64_t>, uint64_t> lastSeen;
+    std::unordered_set<std::pair<uint64_t, uint64_t> > cacheable;
+
+    while(traceFile >> time >> id >> size) {
+        const auto idSize = std::make_pair(id,size);
+        if(size > 0) {
+            lastSeen[idSize]++;
+        }
+        totalreqc++;
+    }
+       
+    for(auto it: lastSeen) {
+        if(it.second>1) {
+            cacheable.insert(it.first);
+        }
+    }
+
+    //reset
+    std::cerr << "lines: " << totalreqc << " objects " << lastSeen.size() << "\n";
+    trace.reserve(totalreqc - lastSeen.size());
+    lastSeen.clear();
+    reqc=0;
+    traceFile.clear();
+    traceFile.seekg(0, std::ios::beg);
+    uniqCount = 0;
+
+    while(traceFile >> time >> id >> size) {
+        const auto idSize = std::make_pair(id,size);
+        if(lastSeen.count(idSize)>0) {
+            assert(cacheable.count(idSize)>0); //just to be sure
+            trace[lastSeen[idSize]].nextSeen = reqc;
+        } else {
+            if(cacheable.count(idSize)>0) {
+                uniqCount++;
+            }
+        }
+        // only add if object is cacheable
+        if(cacheable.count(idSize)>0) {
+            assert(size>0);
+            trace.emplace_back(size);
+            lastSeen[idSize]=reqc++;
+        }
+    }
+    return totalreqc;
+}
